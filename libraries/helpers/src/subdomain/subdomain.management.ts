aac3f5b507b52719a722d132f5b8b614e4347cbf
farhan <syedfarhanahmad0@gmail.com>
2026-03-04 06:19:10 +0530

    fixed cookie auth token logout bug

--- libraries/helpers/src/subdomain/subdomain.management.ts
+++ libraries/helpers/src/subdomain/subdomain.management.ts
@@ -1,6 +1,9 @@
-import { parse } from 'tldts';
+import { parse } from "tldts"
 
 export function getCookieUrlFromDomain(domain: string) {
-  const url = parse(domain);
-  return url.domain! ? '.' + url.domain! : url.hostname!;
+  if (process.env.FRONTEND_DOMAIN) {
+    return process.env.FRONTEND_DOMAIN
+  }
+  const url = parse(domain)
+  return url.domain! ? "." + url.domain! : url.hostname!
 }
